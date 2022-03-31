0df0f56078a74cbeddc6cfff73804a6ad738b936
chzhrr <chzhrr@gmail.com>
2022-03-31 16:07:41 +0800

    Initial Commit.

---
+++ Source/HarmonyPatcher.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace PipetteTool
+{
+    [StaticConstructorOnStartup]
+    internal static class HarmonyPatcher
+    {
+        static HarmonyPatcher()
+        {
+            Harmony instance = new Harmony("Telardo.PipetteTool");
+            instance.PatchAll();
+        }
+    }
+}
---
+++ Source/HotSwappableAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PipetteTool
+{
+    // hot swap attribute for hot-swapping debug
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class HotSwappableAttribute : Attribute
+    {
+    }
+}
