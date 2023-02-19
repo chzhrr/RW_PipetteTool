2513e81909c8f27bf71fbe45ec571e64297b2a32
chzhrr <chzhrr@gmail.com>
2023-02-18 21:35:07 -0800

    v1.1.0 update.
    - Self-learning: Now designators' queue will be dynamically sorted as player's usage.
    - Add build copy command to designators' queue.
    - Small performance boost by removing unavailable designators from the queue.

--- Source/Patch_MapInterface.cs
+++ Source/Patch_MapInterface.cs
@@ -14,23 +14,57 @@ namespace PipetteTool
     [UsedImplicitly]
     public static class Patch_MapInterface
     {
-        // copied from vanilla GizmoGridDrawer
+        #region members
+
+        /// <summary>
+        /// Copied from vanilla GizmoGridDrawer, ordering in gizmo button rendering.
+        /// </summary>
         private static readonly Func<Gizmo, Gizmo, int> SortByOrder = (lhs, rhs) => lhs.Order.CompareTo(rhs.Order);
 
-        // all designators in the database
+        /// <summary>
+        /// All designators in the database plus Forbid, UnForbid and BuildCopy.
+        /// </summary>
         private static List<Designator> s_allAllowedDesignators;
 
-        // current activated designator
+        private static List<Designator> s_allowedDesignatorsInCurrentCycle;
+
+        /// <summary>
+        /// Self learning, we want to minimize total efforts (hot-key pressed times).
+        /// </summary>
+        private static Dictionary<Designator, int> s_hotkeyPressedTimesByDesignators;
+
+        /// <summary>
+        /// Pressed times in this cycle.
+        /// </summary>
+        private static int s_hotkeyPressedTimesInThisCycle;
+
+        /// <summary>
+        /// Current activated designator.
+        /// </summary>
         private static Designator s_currentDesignator;
 
-        // temp list for selectable items at mouse position
-        private static readonly List<Thing> SelectableList = new List<Thing>();
+        /// <summary>
+        /// Temp list for selectable items at mouse position.
+        /// </summary>
+        private static readonly List<Thing> s_selectableList = new List<Thing>();
 
-        // last operated thing
+        /// <summary>
+        /// Last operated thing.
+        /// </summary>
         private static string s_cachedThingId;
 
-        // index of last activated designator
+        /// <summary>
+        /// ThingDef of last operated thing, used to distinguish from standard Place command
+        /// or place command in cycle.
+        /// </summary>
+        private static ThingDef s_cachedThingDef;
+
+        /// <summary>
+        /// Index of last activated designator + 1.
+        /// </summary>
         private static int s_searchStartingIndex;
+
+        #endregion
 
         [UsedImplicitly]
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -49,8 +83,8 @@ namespace PipetteTool
                 Find.Selector.NumSelected == 0
                 // no tab is activated
                 && Find.MainTabsRoot.OpenTab == null
-                // Q is the default rotate hot key when holding an item to place
-                && !(Find.DesignatorManager.SelectedDesignator is Designator_Place))
+                // avoid conflict with rotate left designator with default hot-key Q
+                && NotPlaceCommandOrInCycle())
             {
                 // if we press the hot key Q
                 if (CustomKeyBindingDefOf.PipetteToolHotKey.KeyDownEvent)
@@ -60,8 +94,7 @@ namespace PipetteTool
                     {
                         ResolveAllDesignators();
                     }
-                    GetSelectableListUnderMouse();
-                    Thing thing = SelectableList.FirstOrDefault();
+                    Thing thing = GetFirstThingUnderMouse();
                     if (thing == null)
                     {
                         return;
@@ -69,20 +102,45 @@ namespace PipetteTool
                     // if current thing is not cached or has a designation
                     if (thing.ThingID != s_cachedThingId || thing.Map?.designationManager?.DesignationOn(thing) != null)
                     {
-                        // start searching at first
+                        // start a new cycle
                         s_searchStartingIndex = 0;
+                        s_hotkeyPressedTimesInThisCycle = 0;
+                        s_allAllowedDesignators.SortStable(CompareDesignatorEfforts);
+                        s_allowedDesignatorsInCurrentCycle = new List<Designator>(s_allAllowedDesignators);
+                        if (thing is Building building)
+                        {
+                            Designator_Build buildCopyDesignator = GetBuildCopyDesignator(building);
+                            if (buildCopyDesignator != null)
+                            {
+                                s_allowedDesignatorsInCurrentCycle.Insert(0, buildCopyDesignator);
+                            }
+                        }
+                        s_allowedDesignatorsInCurrentCycle.SortStable(CompareDesignatorEfforts);
                     }
                     s_currentDesignator = GetNextAllowedDesignator(thing);
                     if (s_currentDesignator != null)
                     {
                         Find.DesignatorManager.Select(s_currentDesignator);
+                        // don't count build copy command,
+                        // they will always be the first command in the cycle
+                        if (!(s_currentDesignator is Designator_Build))
+                        {
+                            s_hotkeyPressedTimesByDesignators[s_currentDesignator] += s_hotkeyPressedTimesInThisCycle;
+                        }
                     }
+                    // if there is no other allowed designator, deselect current one
                     else
                     {
                         Find.DesignatorManager.Deselect();
                     }
-                    SelectableList.Clear();
+                    s_selectableList.Clear();
                 }
+            }
+
+            bool NotPlaceCommandOrInCycle()
+            {
+                return !(Find.DesignatorManager.SelectedDesignator is Designator_Place des)
+                       || (s_cachedThingDef != null && s_cachedThingDef == des?.PlacingDef);
             }
 
             void ResolveAllDesignators()
@@ -99,45 +157,11 @@ namespace PipetteTool
                 // inspired by GizmoGridDrawer.DrawGizmoGrid
                 // same order as gizmos' drawing order
                 s_allAllowedDesignators.SortStable(SortByOrder);
+                s_hotkeyPressedTimesByDesignators = Enumerable.Range(0, s_allAllowedDesignators.Count).
+                    ToDictionary(i => s_allAllowedDesignators[i], i => 0);
             }
 
-            Designator GetNextAllowedDesignator(Thing thing)
-            {
-                // If we have activate one designator for this item,
-                // we should activate the next allowed designator next time we press the hot key.
-                // Otherwise, restart from the first allowed one.
-                s_cachedThingId = thing.ThingID;
-                for (int i = s_searchStartingIndex; i < s_allAllowedDesignators.Count; i++)
-                {
-                    Designator designator = s_allAllowedDesignators[i];
-                    AcceptanceReport acceptanceReport = designator.CanDesignateThing(thing);
-                    if (acceptanceReport.Accepted)
-                    {
-                        // next time we should start from the next designator
-                        s_searchStartingIndex = i + 1;
-                        return designator;
-                    }
-                }
-                // when switching items, reset
-                s_searchStartingIndex = 0;
-                return null;
-            }
-
-            void GetSelectableListUnderMouse()
-            {
-                foreach (Thing thing in Find.CurrentMap.thingGrid.ThingsAt(IntVec3.FromVector3(UI.MouseMapPosition())))
-                {
-                    // exempt: not selectable or under fog
-                    if (ThingSelectionUtility.SelectableByMapClick(thing))
-                    {
-                        SelectableList.Add(thing);
-                    }
-                }
-                // higher altitude -> lower altitude
-                SelectableList.Sort(CompareThingsByDrawAltitude);
-            }
-
-            // We sort things list in ascending order,
+            // We sort things list in descending order,
             // thing at higher altitude is smaller in this comparison.
             int CompareThingsByDrawAltitude(Thing thingA, Thing thingB)
             {
@@ -151,6 +175,103 @@ namespace PipetteTool
                 }
                 return -1;
             }
+
+            Thing GetFirstThingUnderMouse()
+            {
+                foreach (Thing thing in Find.CurrentMap.thingGrid.ThingsAt(IntVec3.FromVector3(UI.MouseMapPosition())))
+                {
+                    // exempt: not selectable or under fog
+                    if (ThingSelectionUtility.SelectableByMapClick(thing))
+                    {
+                        s_selectableList.Add(thing);
+                    }
+                }
+                // higher altitude -> lower altitude
+                s_selectableList.Sort(CompareThingsByDrawAltitude);
+                return s_selectableList.FirstOrDefault();
+            }
+
+            Designator GetNextAllowedDesignator(Thing thing)
+            {
+                // If we have activate one designator for this item,
+                // we should activate the next allowed designator next time we press the hot key.
+                // Otherwise, restart from the first allowed one.
+                s_cachedThingId = thing.ThingID;
+                s_cachedThingDef = thing.def;
+                for (int i = s_searchStartingIndex; i < s_allowedDesignatorsInCurrentCycle.Count; i++)
+                {
+                    Designator designator = s_allowedDesignatorsInCurrentCycle[i];
+                    AcceptanceReport acceptanceReport = designator.CanDesignateThing(thing);
+                    if (acceptanceReport.Accepted || designator is Designator_Build)
+                    {
+                        // next time we should start from the next designator
+                        s_searchStartingIndex = i + 1;
+                        s_hotkeyPressedTimesInThisCycle += 1;
+                        return designator;
+                    }
+                    s_allowedDesignatorsInCurrentCycle.RemoveAt(i);
+                    i--;
+
+                }
+                // restart the cycle if run out of all designators
+                s_searchStartingIndex = 0;
+                return null;
+            }
+
+            int CompareDesignatorEfforts(Designator lhs, Designator rhs)
+            {
+                // make sure build copy command is the first in the cycle
+                if (!s_hotkeyPressedTimesByDesignators.ContainsKey(lhs))
+                {
+                    return -1;
+                }
+                if (!s_hotkeyPressedTimesByDesignators.ContainsKey(rhs))
+                {
+                    return 1;
+                }
+
+                if (lhs is Designator_Cancel)
+                {
+                    return -1;
+                }
+                // designator with more pressed times comes first in the cycle
+                // we want to minimize total pressed times
+                if (s_hotkeyPressedTimesByDesignators[lhs] > s_hotkeyPressedTimesByDesignators[rhs])
+                {
+                    return -1;
+                }
+                // if tied, use gizmo rendering order
+                return s_hotkeyPressedTimesByDesignators[lhs] == s_hotkeyPressedTimesByDesignators[rhs] ? SortByOrder(lhs, rhs) : 1;
+            }
+
+            Designator_Build GetBuildCopyDesignator(Building building)
+            {
+                Designator_Build des = BuildCopyCommandUtility.FindAllowedDesignator(building.def);
+                if (des == null)
+                {
+                    return null;
+                }
+                if (building.def.MadeFromStuff && building.Stuff == null)
+                {
+                    return des;
+                }
+                ColorInt? glowerColorOverride = null;
+                CompGlower comp;
+                if ((comp = building.GetComp<CompGlower>()) != null && comp.HasGlowColorOverride)
+                {
+                    glowerColorOverride = comp.GlowColor;
+                }
+                des.glowerColorOverride = glowerColorOverride;
+                des.SetTemporaryVars(building.Stuff, true);
+                ThingDef stuffDefRaw = des.StuffDefRaw;
+                des.SetStuffDef(building.Stuff);
+                des.styleDef = building.StyleDef;
+                des.sourcePrecept = building.StyleSourcePrecept as Precept_Building;
+                // TODO: side effect?
+                // des.SetStuffDef(stuffDefRaw);
+                return des;
+            }
+
         }
     }
 }
